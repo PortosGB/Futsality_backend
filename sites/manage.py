9bbfae5d90d2432512651ec74161a38a55f80dc5
Quentin Durand <jayecon96@gmail.com>
2020-02-29 23:16:31 +0100

    adding router + refactoring manage

--- sites/manage.py
+++ sites/manage.py
@@ -2,25 +2,19 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from .exts import db
 from .models import User
+from .urls import router
 
 
-app = Flask(__name__)
-app.config.from_object('config')
-db.init_app(app)
+def register_extensions(_app):
+    _app.register_blueprint(router)
+    db.init_app(_app)
 
 
+def create_app():
+    _app = Flask(__name__)
+    _app.config.from_object('config')
+    register_extensions(_app)
+    return _app
 
-@app.route('/')
-def index():
-    users = User.query.all()
 
-    output = []
-
-    for user in users:
-        user_data = {}
-        user_data['firstname'] = user.firstname
-        user_data['password'] = user.password
-        user_data['admin'] = user.admin
-        output.append(user_data)
-    return jsonify({'users': output})
-
+app = create_app()
---
+++ sites/urls.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify
+from .models import User
+
+router = Blueprint('router', __name__)
+
+
+@router.route('/')
+def index():
+    users = User.query.all()
+
+    output = []
+
+    for user in users:
+        user_data = {}
+        user_data['firstname'] = user.firstname
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+    return jsonify({'users': output})
